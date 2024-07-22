e0bb56dbafe7bb5145e48aad31d8d4cd7ad7e0d9
Cristian Bustamante <cjb.364@gmail.com>
2024-07-22 14:04:27 -0300

    3 Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/OnlineStore/",
+  base: "https://cristianjbustamante.github.io/OnlineStore/",
   plugins: [react()],
 })
